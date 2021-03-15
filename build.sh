43feb9a43b9fc6f45c27284853d20b53c46f1f38
Luddifee <ludwig@ludwig-lehnert.de>
2021-03-15 11:06:17 +0100

    stripping the output binary now

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ cd src
 flex --outfile=lex.yy.cc lexer.ll
 bison parser.yy
 
-clang++ -Wextra -Wall -lfl main.cc utils.cc lex.yy.cc parser.tab.cc -lm -O3 -o ../plsm
+clang++ -Wextra -Wall -lfl main.cc utils.cc lex.yy.cc parser.tab.cc -lm -O3 -s -o ../plsm
