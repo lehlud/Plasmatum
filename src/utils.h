f1290ee87e70d427679a45037ce2d55489e629d0
Luddifee <ludwig@ludwig-lehnert.de>
2021-06-08 18:25:48 +0200

    general update

--- src/utils.h
+++ src/utils.h
@@ -2,18 +2,14 @@
 
 #include <string>
 
-namespace Plasmatum
-{
-    namespace Utils
-    {
+namespace Plasmatum {
+namespace Utils {
 
-        template <typename Base, typename T>
-        inline bool instanceof (const T *)
-        {
-            return std::is_base_of<Base, T>::value;
-        }
-
-        std::string readFile(std::string name);
-
-    }
+template <typename Base, typename T> inline bool instanceof (const T *) {
+  return std::is_base_of<Base, T>::value;
 }
+
+std::string readFile(std::string name);
+
+} // namespace Utils
+} // namespace Plasmatum
